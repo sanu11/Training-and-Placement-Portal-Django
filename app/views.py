135c4253df2b592fbfde770c7ac2b224378b38c4
sanu11 <sanikashah1110@gmail.com>
2016-08-14 01:22:45 +0530

    update

--- app/views.py
+++ app/views.py
@@ -5,23 +5,26 @@ from django.http import HttpResponse
 from django.conf import settings
 from datetime import datetime
 from django.views.decorators.csrf import *
+import json
 def index(request):
    return render(request,'app/register.html',{})        
 
 @csrf_exempt
 def register_student(request):
-	if(request.method=="POST"):
+ 	if request.is_secure():
+		checkin_json = request.POST['body']
+		pair= json.loads(request.POST)
 		obj=Student()
-		obj.user=request.POST.get("name")
-		obj.email=request.POST.get("email")
-		obj.password=request.POST.get("password")
-		obj.address=request.POST.get("address")
-		obj.phone=request.POST.get("phone")
-		obj.branch=request.POST.get("branch")
-		obj.average=request.POST.get("average")
-		obj.placed=request.POST.get("placed")
-		obj.active_back=request.POST.get("active_back")
-		obj.num_back=request.POST.get("num_back")
+		obj.user=pair["name"]
+		obj.email=pair["email"]
+		obj.password=pair["password"]
+		obj.address=pair["address"]
+		obj.phone=pair["phone"]
+		obj.branch=pair["branch"]
+		obj.average=pair["average"]
+		obj.placed=pair["placed"]
+		obj.active_back=pair["active_back"]
+		obj.num_back=pair["num_back"]
 		# print obj.user ,obj.email,obj.password,obj.address,obj.phone,obj.branch,obj.average,obj.placed,obj.num_back,obj.active_back
 		obj.save()
 		return HttpResponse("Data saved")
