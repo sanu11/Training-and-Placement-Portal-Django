9dbc399933f459db82546004d2639798308f5688
sanu11 <sanikashah1110@gmail.com>
2016-08-15 12:51:00 +0530

    db_changed

--- Tnp/settings.py
+++ Tnp/settings.py
@@ -110,14 +110,14 @@ TEMPLATES = [
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.mysql',
+#         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
-#         # 'NAME': 'Tnp',
-#         # 'USER': 'root',
-#         # 'PASSWORD': 'jerrysan',
-#         # 'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-#         # 'PORT': '3306',
+#         'NAME': 'Tnp',
+#         'USER': 'root',
+#         'PASSWORD': 'jerrysan',
+#         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+#         'PORT': '3306',
 #     }
 # }
 
