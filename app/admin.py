d0f77d18071dcf0f7f7ef7ec8e8aa16942038dd1
sanu11 <sanikashah1110@gmail.com>
2016-09-14 21:41:04 +0530

    gcm

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Student,Company
+from .models import Student,Company,Message
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Company)
+admin.site.register(Message)
+admin.site.register(gcm_device)
+
+
 
