f0ad7f9a616fb5ec2e88776c2d549de9a257cb7d
sanu11 <sanikashah1110@gmail.com>
2016-12-02 00:48:50 +0530

    company update

--- app/views.py
+++ app/views.py
@@ -65,7 +65,7 @@ def update_company(request):
 	obj.reg_start_date=reg_start
 	obj.reg_end_date=reg_end
 	obj.reg_link=reg_link
-	if(other_details):
+	if(obj.other_details):
 		obj.other_details=obj.other_details + " " +  other_details
 	else:
 		obj.other_details=other_details
