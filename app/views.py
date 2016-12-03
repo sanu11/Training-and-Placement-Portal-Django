a6ee30c7b430ba032b93dd4075348b50679c9cb1
sanu11 <sanikashah1110@gmail.com>
2016-12-03 15:29:57 +0530

    update company

--- app/views.py
+++ app/views.py
@@ -16,6 +16,9 @@ def index(request):
 def register_student(request):
 	data = json.loads(request.body)
 	obj=Student()
+	name = data["name"]
+	if(Student.objects.filter(name=name).exists()):
+		return HttpResponse("Already Registered")
 	obj.user=data["name"]
 	obj.email=data["email"]
 	obj.password=data["password"]
@@ -25,51 +28,45 @@ def register_student(request):
 	obj.placed=data["placed"]
 	obj.active_back=data["activeBack"]
 	obj.save()
-	return HttpResponse("Data saved")
+	return HttpResponse("Registered Successfully")
 
 @csrf_exempt
 def register_company(request):
 	data = json.loads(request.body)
 	name=data["name"]	
 	if(Company.objects.filter(name=name).exists()):
-		return HttpResponse("Company exists")
+		return HttpResponse("Already Registered")
 
-	criteria = data["criteria"]
-	salary=data["salary"]
-	other_details=data["other_details"]
-	ppt_date=data["ppt_date"]
-	back=data["back"]
-
-	#add to database
 	obj=Company()
-	obj.name=name
-	obj.criteria=criteria
-	obj.salary=salary
-	obj.other_details=other_details
-	obj.ppt_date=ppt_date
-	obj.back=back
+	obj.name=data["name"]
+	obj.criteria=data["criteria"]
+	obj.salary=data["salary"]
+	obj.other_details=data["other_details"]
+	obj.ppt_date=data["ppt_date"]
+	obj.back=data["back"]
 	obj.save()
 
 	#send notification
 	Device = get_device_model()
 	Device.objects.all().send_message({'type':'company_reg','name':name,'criteria':criteria,'salary':salary,'other_details':other_details,'ppt_date':ppt_date,'back':back})
-	return HttpResponse("Data saved")
+	return HttpResponse("Registered Successfully")
 
 @csrf_exempt
 def update_company(request):
 	data = json.loads(request.body)	
 	name=data["name"]
-	reg_start=data["reg_start"]
-	reg_end=data["reg_end"]
+	
 	reg_link=data["reg_link"]
 	other_details=data["other_details"]
 
 	obj=Company.objects.get(name=name)
 	if(not obj.reg_link):
 		return HttpResponse("Already Updated")
-	obj.reg_start_date=reg_start
-	obj.reg_end_date=reg_end
+
 	obj.reg_link=reg_link
+	obj.reg_start_date=data["reg_start"]
+	obj.reg_end_date=data["reg_end"]
+	
 	if(obj.other_details and other_details):
 		obj.other_details=obj.other_details + " " +  other_details
 	elif(other_details):
