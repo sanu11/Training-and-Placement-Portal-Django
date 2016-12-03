f70cdb586dd4836113c5cfc329912e3470006df9
sanu11 <sanikashah1110@gmail.com>
2016-12-03 15:37:12 +0530

    update company

--- app/views.py
+++ app/views.py
@@ -37,13 +37,20 @@ def register_company(request):
 	if(Company.objects.filter(name=name).exists()):
 		return HttpResponse("Already Registered")
 
+	criteria = data["criteria"]
+	salary=data["salary"]
+	other_details=data["other_details"]
+	ppt_date=data["ppt_date"]
+	back=data["back"]
+
+	#add to database
 	obj=Company()
-	obj.name=data["name"]
-	obj.criteria=data["criteria"]
-	obj.salary=data["salary"]
-	obj.other_details=data["other_details"]
-	obj.ppt_date=data["ppt_date"]
-	obj.back=data["back"]
+	obj.name=name
+	obj.criteria=criteria
+	obj.salary=salary
+	obj.other_details=other_details
+	obj.ppt_date=ppt_date
+	obj.back=back
 	obj.save()
 
 	#send notification
@@ -55,18 +62,17 @@ def register_company(request):
 def update_company(request):
 	data = json.loads(request.body)	
 	name=data["name"]
-	
+	reg_start=data["reg_start"]
+	reg_end=data["reg_end"]
 	reg_link=data["reg_link"]
 	other_details=data["other_details"]
 
 	obj=Company.objects.get(name=name)
 	if(not obj.reg_link):
 		return HttpResponse("Already Updated")
-
+	obj.reg_start_date=reg_start
+	obj.reg_end_date=reg_end
 	obj.reg_link=reg_link
-	obj.reg_start_date=data["reg_start"]
-	obj.reg_end_date=data["reg_end"]
-	
 	if(obj.other_details and other_details):
 		obj.other_details=obj.other_details + " " +  other_details
 	elif(other_details):
