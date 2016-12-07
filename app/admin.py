1f438ae20804bece699dd565f8b979b4f4600eb0
sanu11 <sanikashah1110@gmail.com>
2016-12-07 16:12:43 +0530

    verification

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Student,Company,Message
+from .models import Student,Company,Message,Verify
 
 # Register your models here.
 admin.site.register(Student)
