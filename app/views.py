3aa2c79755ada81ff15f6b3688b4c0c852924bef
sanu11 <sanikashah1110@gmail.com>
2016-12-28 13:16:49 +0530

    changed url sequence

--- app/views.py
+++ app/views.py
@@ -243,6 +243,7 @@ def web_register_company(request):
 		back=request.POST["back"]
 		ppt_date=request.POST["ppt_date"]
 		other_details=request.POST["other_details"]
+		print name," ",salary," ",other_details
  #add to database
 		obj=Company()
 		obj.name=name
