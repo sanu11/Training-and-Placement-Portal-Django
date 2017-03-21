4f2d0105d6af41c70d21a8ac79579ecccba9c431
sanu11 <sanikashah1110@gmail.com>
2017-03-21 17:14:47 +0530

    developers added to login page

--- app/views.py
+++ app/views.py
@@ -171,7 +171,7 @@ def get_developers_page(request):
     else:
         get_mail = request.session["email"]
         if Admin.objects.filter(email=get_mail).exists():
-            login = 1`
+            login = 1
             print "Admin login"
             name = request.session["name"]
         
