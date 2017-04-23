8955198697ec6279f3214365bd533914200c296b
sanu11 <sanikashah1110@gmail.com>
2017-04-24 03:25:45 +0530

    id added to admin

--- app/admin.py
+++ app/admin.py
@@ -27,7 +27,7 @@ class StudentAdmin(ImportExportModelAdmin):
 @admin.register(Company)
 class CompanyAdmin(ImportExportModelAdmin):
     resource_class = CompanyResource
-    readonly_fields=('id',)
+    readonly_fields=('c_id',)
 
 @admin.register(Message)
 class MessageAdmin(ImportExportModelAdmin):
