9ced2d63693407180e24a4b3ea19aede8a72efba
sanu11 <sanikashah1110@gmail.com>
2017-06-28 13:53:32 +0530

    manage option added to ui

--- app/urls.py
+++ app/urls.py
@@ -28,6 +28,14 @@ urlpatterns = [
 	url(r'^presult/$',views.get_result_upload_page),	 
 	url(r'^psearch/$',views.get_search_student_page),
 
+
+    url(r'^cregister/$',views.web_register_company),
+	url(r'^update/$',views.web_update_company),
+	url(r'^editCompany/$', views.web_edit_company),
+	url(r'^getCompanyDetails/$',views.get_company_details),
+	url(r'^notify/$',views.web_notify),
+	url(r'^result/$',views.web_upload_result),
+
     url(r'^roll/$',views.get_student_details),
 	url(r'^lockStudent/$',views.web_lock_student),
 	url(r'^unlockStudent/$',views.web_unlock_student),
@@ -42,17 +50,19 @@ urlpatterns = [
 	url(r'^lockCompany/$', views.web_lock_company),
     url(r'^placedStudents/$', views.web_placed_students),
 
-    url(r'^cregister/$',views.web_register_company),
-	url(r'^update/$',views.web_update_company),
-	url(r'^editCompany/$', views.web_edit_company),
-	url(r'^getCompanyDetails/$',views.get_company_details),
-	url(r'^notify/$',views.web_notify),
-	url(r'^result/$',views.web_upload_result),
-
 ###student side get pages
 	url(r'^psignup/$',views.get_signup_page),		
 	url(r'^plogin/$',views.get_login_page),	
 	url(r'^psettings/$',views.get_settings_page),
+
+
+
+    url(r'^signup/$',views.web_signup),
+	url(r'^login/$',views.web_login),
+	url(r'^changepassword/$', views.web_change_password),
+	url(r'^resume/$',views.web_upload_resume),
+    url(r'^applycompany/$', views.web_apply_company),
+    url(r'^updatemarks/$', views.web_update_marks),
 
 	##profile pages
 	url(r'^peditprofile/$',views.get_edit_profile_page),
@@ -63,16 +73,6 @@ urlpatterns = [
 	url(r'^peditotherdetails/$',views.get_edit_other_details_page),
 	url(r'^presume/$',views.get_resume_upload_page),
 
-	url(r'^pstudentdownload/$',views.get_student_download_page),
-    url(r'^pupdatemarks/$', views.get_update_marks_page),
-
-    url(r'^signup/$',views.web_signup),
-	url(r'^login/$',views.web_login),
-	url(r'^changepassword/$', views.web_change_password),
-	url(r'^resume/$',views.web_upload_resume),
-    url(r'^applycompany/$', views.web_apply_company),
-    url(r'^updatemarks/$', views.web_update_marks),
-
     url(r'^editprofile/$', views.web_edit_profile),
 	url(r'^editsscmarks/$', views.web_edit_ssc_marks),
 	url(r'^edithscmarks/$', views.web_edit_hsc_marks),
@@ -80,12 +80,16 @@ urlpatterns = [
 	url(r'^editmemarks/$', views.web_edit_me_marks),
 	url(r'^editotherdetails/$', views.web_edit_other_details),
 
+	url(r'^pstudentdownload/$',views.get_student_download_page),
+    url(r'^pupdatemarks/$', views.get_update_marks_page),
+
+
 ##download pages
 	url(r'^downloadstudents/$', views.web_download_students),
 	url(r'^downloadcompanies/$', views.web_download_companies),
 	url(r'^downloadappliedcompanies/$', views.web_download_applied_students),
 
-	##display pages
+##display pages
 	url(r'^results/$',views.get_results_page),	 
 	url(r'^companies/$',views.get_companies_page),
 	url(r'^opportunities/$',views.get_opportunities_page),	 
@@ -100,6 +104,5 @@ urlpatterns = [
 
 	url(r'^$', views.get_main_page),
 	url(r'^developers-page/$', views.get_developers_page),
-
 
 ]
